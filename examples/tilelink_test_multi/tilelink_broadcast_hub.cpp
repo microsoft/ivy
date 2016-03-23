55262037753f19689eff4036c47d142a1c06e68c
Ken McMillan <kenmcmil@microsoft.com>
2016-03-22 17:33:21 -0700

    adding forgotten file

---
+++ examples/tilelink_test_multi/tilelink_broadcast_hub.cpp
@@ -0,0 +1,11 @@
+
+
+#include "L2BroadcastHub.h"
+#include "L2BroadcastHub.prms.h"
+
+// typedef L2BroadcastHub_t L2Unit_t;
+
+#define L2Unit L2BroadcastHub
+
+#include "tilelink_coherence_manager_bcast.cpp"
+
