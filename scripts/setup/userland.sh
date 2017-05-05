a0eefd1c49d0d5debee6fee7a94483cee612cf5f
Michael Lowell Roberts <mirobert@microsoft.com>
2017-05-05 13:30:14 -0700

    rename `scripts/setup/userspace.sh` to `.../userland.sh`.

--- scripts/setup/userland.sh
+++ scripts/setup/userland.sh
@@ -15,8 +15,8 @@ gitroot=$(git rev-parse --show-toplevel)
 
 $SHELL $gitroot/scripts/setup/python.sh
 
-if [ "x$1" == "x--vagrant" ] && ! grep -q 'cd /vagrant' $HOME/.profile; then
+if [ "x$1" = "x--vagrant" ] && ! grep -q 'cd /vagrant' $HOME/.profile; then
    echo 'cd /vagrant' >> $HOME/.profile
-   echo 'eval "$(sh /vagrant/scripts/setup/python.sh env)"' >> $HOME/.profile
+   echo 'eval "export PATH=$HOME/.local/bin:$PATH"' >> $HOME/.profile
    echo 'eval "$(sh /vagrant/scripts/setup/z3.sh env)"' >> $HOME/.profile
 fi
