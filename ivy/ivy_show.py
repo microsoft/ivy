b9219eafd0242995a6218830776b776bf8ee2f3a
Ken McMillan <kenmcmil@microsoft.com>
2017-07-13 15:04:59 -0700

    fixed module reference in ivy_show

--- ivy/ivy_show.py
+++ ivy/ivy_show.py
@@ -15,6 +15,7 @@ import ivy_art
 import ivy_interp
 import ivy_compiler
 import ivy_isolate
+import ivy_init
 
 import sys
 
@@ -35,13 +36,13 @@ def check_module():
 
 
 def main():
-    ivy.read_params()
+    ivy_init.read_params()
     iu.set_parameters({'show_compiled':'true'})
     if len(sys.argv) != 2 or not sys.argv[1].endswith('ivy'):
         usage()
     with im.Module():
         with utl.ErrorPrinter():
-            ivy.source_file(sys.argv[1],ivy.open_read(sys.argv[1]),create_isolate=False)
+            ivy_init.source_file(sys.argv[1],ivy_init.open_read(sys.argv[1]),create_isolate=False)
             check_module()
 
 
