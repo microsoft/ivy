decdedc7122e6beb89a1157050388e7678e7039b
Egor Bulychev <egorbu@gmail.com>
2018-10-18 23:41:58 +0200

    Update setup.py
    * add a long description from README
    * use codecs to prevent the crash during the installation on machines with non-standard locales.

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+
+# Get the long description from the README file
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+  with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+      long_description = f.read()
+except:
+  # This happens when running tests
+  long_description = None
 
 setup(name='ms_ivy',
       version='0.1',
       description='IVy verification tool',
+      long_description=long_description,
       url='https://github.com/microsoft/ivy',
       author='IVy team',
       author_email='nomail@example.com',
