175043b6a15da2a3ad5bd1668b3fb16aba2e17d9
Ken McMillan <kenmcmil@microsoft.com>
2019-09-11 15:23:36 -0700

    adding submodule build

---
+++ build_submodules.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import platform
+if platform.system() == 'Windows':
+    print "windows build is not currently supported"
+    exit(1)
+
+def do_cmd(cmd):
+    print cmd
+    status = os.system(cmd)
+    if status:
+        exit(1)
+    
+cwd = os.getcwd()
+
+if not os.path.exists('submodules/z3'):
+    print "submodules/z3 not found. try 'git submodule update; git submodule update'"
+    exit(1)
+
+os.chdir('submodules/z3')
+
+ivydir = os.path.join(cwd,'ivy')
+
+cmd = 'python scripts/mk_make.py --python --prefix {} --pypkgdir {}/'.format(cwd,ivydir)
+
+do_cmd(cmd)
+
+os.chdir('build')
+
+do_cmd('make -j 4')
+
+do_cmd('make install')
+
+
+
+
