da91befff0da4b0a176f6fb7dd0b253b34e990d6
Ken McMillan <kenmcmil@microsoft.com>
2020-04-18 19:20:07 -0700

    issue #60

--- ivy/ivy_transrel.py
+++ ivy/ivy_transrel.py
@@ -30,7 +30,7 @@ that all symbols *not* in S are preserved.
 
 from ivy_utils import UniqueRenamer, union_to_list, list_union, list_diff, IvyError, inverse_map, compose_maps, pretty
 from ivy_logic import Variable, Constant, Literal, Atom, Not, And, Or,App, RelationSort, Definition, is_prenex_universal
-from ivy_logic_utils import used_symbols_clauses, rename_clauses, clauses_using_symbols, simplify_clauses,\
+from ivy_logic_utils import symbols_clauses, used_symbols_clauses, rename_clauses, clauses_using_symbols, simplify_clauses,\
     used_variables_clauses, used_constants_clauses, substitute_constants_clause, substitute_constants_clauses, constants_clauses,\
     relations_clauses, eq_lit, condition_clauses, or_clauses, ite_clauses, and_clauses, false_clauses, true_clauses,\
     formula_to_clauses, clauses_to_formula, formula_to_clauses_tseitin, is_ground_clause, \
@@ -310,6 +310,8 @@ def compose_updates(update1,axioms,update2):
     mid = us1.intersection(us2)
     mid_ax = clauses_using_symbols(mid,axioms)
     used = used_symbols_clauses(and_clauses(clauses1,clauses2))
+    used.update(symbols_clauses(pre1))
+    used.update(symbols_clauses(pre2))
     rn = UniqueRenamer('__m_',used)
     map1 = dict()
     map2 = dict()
